24287c0b752203b56f9bac225732e3b199afd264
Gero Rawert <gerorawert@example.com>
2025-06-07 19:54:58 +0200

    update mobil screen dynamik fit

--- js/config.js
+++ js/config.js
@@ -1,34 +1,35 @@
 // js/config.js
 
-const GAME_AREA_WIDTH = 600;
-const GAME_AREA_HEIGHT = 400;
+const GAME_AREA_WIDTH = window.innerWidth;
+const GAME_AREA_HEIGHT = window.innerHeight;
 
 const gameSettings = {
     gravity: 0.7,
     pushForce: 0.5,
     maxPlayerDX: 3,
     worldScrollSpeed: 2.5,
-    jumpForce: 14.5,     // Ggf. leicht erhöht für das neue "Gewicht"
-    playerWidth: 50,     // NEU: Breite der Kollisionsbox (z.B. Skateboard-Breite)
-    playerHeight: 75,    // NEU: Gesamthöhe = Körperhöhe (60) + Boardhöhe (15)
+    jumpForce: 14.5,
+    playerWidth: GAME_AREA_WIDTH * 0.08,  // z. B. 8% der Breite
+    playerHeight: GAME_AREA_HEIGHT * 0.19, // z. B. 19% der Höhe
     railHeight: 8,
     ollieScore: 10,
     grindScorePerFrame: 1,
     grindAdjustSpeed: 1.5,
-    groundLevelY: GAME_AREA_HEIGHT - 30,
     visualPlayerYOffset: 10,
 
-    // Objektgenerierung - diese Werte sind jetzt noch wichtiger anzupassen!
-    minGapBetweenObjects: 80,
-    maxGapBetweenObjects: 190, // Etwas mehr Varianz und größere mögliche Lücken
-    maxHeightDiffUp: 80,       // Muss zur neuen effektiven Sprunghöhe passen
-    maxHeightDiffDown: 100,
-    minPlatformWidth: 100,
-    maxPlatformWidth: 180,
-    minRailWidth: 120,
-    maxRailWidth: 220,
-    objectGenerationThreshold: 180,
-    groundLevelY: GAME_AREA_HEIGHT - 20 - 80,
-    minObjectY: GAME_AREA_HEIGHT - 170 - 80, // Höchste Plattform Oberkante
-    maxObjectY: GAME_AREA_HEIGHT - 40 - 80,   // Niedrigste Plattform Oberkante (außer Boden)
+    // Objektgenerierung (angepasst an Höhe)
+    minGapBetweenObjects: GAME_AREA_WIDTH * 0.12,
+    maxGapBetweenObjects: GAME_AREA_WIDTH * 0.3,
+    maxHeightDiffUp: GAME_AREA_HEIGHT * 0.2,
+    maxHeightDiffDown: GAME_AREA_HEIGHT * 0.25,
+    minPlatformWidth: GAME_AREA_WIDTH * 0.15,
+    maxPlatformWidth: GAME_AREA_WIDTH * 0.3,
+    minRailWidth: GAME_AREA_WIDTH * 0.2,
+    maxRailWidth: GAME_AREA_WIDTH * 0.37,
+    objectGenerationThreshold: GAME_AREA_WIDTH * 0.3,
+
+    // Plattformpositionen relativ zur Spielfläche
+    groundLevelY: GAME_AREA_HEIGHT - (GAME_AREA_HEIGHT * 0.12),
+    minObjectY: GAME_AREA_HEIGHT - (GAME_AREA_HEIGHT * 0.45),
+    maxObjectY: GAME_AREA_HEIGHT - (GAME_AREA_HEIGHT * 0.22),
 };

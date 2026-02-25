6aea7dd2bb8103a1f366c341412a59f965b2e717
Gero Rawert <gerorawert@example.com>
2026-02-25 21:38:02 +0100

    feat: add pixel art icon for leg extension

--- gym-tracker/app.js
+++ gym-tracker/app.js
@@ -65,7 +65,7 @@ const TRAINING_PLAN = {
             { name: "Squat", sets: 3, repRange: [5, 8], rir: "1–2", startWeight: 60, bodyPart: "lower", imageUrl: "assets/images/pixel_squat.png" },
             { name: "RDL", sets: 3, repRange: [8, 12], rir: "2", startWeight: 10, bodyPart: "lower", imageUrl: "assets/images/pixel_rdl.png", note: "langsam - wenig" },
             { name: "Seated Leg Curl", sets: 3, repRange: [10, 15], rir: "2", startWeight: 27.5, bodyPart: "lower", imageUrl: "assets/images/pixel_leg_curl.png", note: "Streckung warten!" },
-            { name: "Leg Extension", sets: 3, repRange: [12, 20], rir: "2", startWeight: 35, bodyPart: "lower", imageUrl: "" },
+            { name: "Leg Extension", sets: 3, repRange: [12, 20], rir: "2", startWeight: 35, bodyPart: "lower", imageUrl: "assets/images/pixel_leg_extension.png" },
             { name: "Nautilus Glute Drive", sets: 2, repRange: [8, 12], rir: "2", startWeight: 50, bodyPart: "lower", imageUrl: "", note: "Hüfte durchstrecken!" },
             { name: "Standing Calf Raise", sets: 4, repRange: [10, 15], rir: "1–2", startWeight: 0, bodyPart: "lower", imageUrl: "assets/images/pixel_calf_raise.png" },
             { name: "Seated Calf Raise", sets: 2, repRange: [12, 20], rir: "2", startWeight: 0, bodyPart: "lower", imageUrl: "assets/images/pixel_calf_raise.png" },
